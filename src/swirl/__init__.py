3ef0b170cc917c7f0a6a65010f709ad7a1c55ef4
HBXK <hklinteback@gmail.com>
2026-08-19 15:53:31 -0700

    Update __init__.py

--- src/swirl/__init__.py
+++ src/swirl/__init__.py
@@ -2,6 +2,8 @@ from .cebra_analysis import CEBRAAnalysis, CEBRAUtils
 from .stratified_gm import (
     compute_stratified_wasserstein_distances,
     distance_to_rbf_median,
+    cluster,
+    plot_umap_from_cluster_indices,
     pairwise_cluster_mmd_precomputed,
     run_analysis,
     split_point_clouds,
